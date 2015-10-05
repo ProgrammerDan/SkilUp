fa8bc2ea1d38f951d532546284323c6b3de41b3d
maxopoly <maxseinbot@gmx.de>
2015-10-05 17:20:33 +0200

    Command kraut and basic main class structure

--- src/main/java/com/github/maxopoly/DankMeme/DankMeme.java
+++ src/main/java/com/github/maxopoly/DankMeme/DankMeme.java
@@ -1,5 +1,27 @@
 package com.github.maxopoly.DankMeme;
 
-public class DankMeme {
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.plugin.java.JavaPlugin;
 
+import com.github.maxopoly.DankMeme.commands.CommandHandler;
+
+public class DankMeme extends JavaPlugin {
+	CommandHandler commandHandler;
+
+	@Override
+	public void onEnable() {
+		commandHandler = new CommandHandler(this);
+	}
+	
+	@Override
+	public void onDisable() {
+		
+	}
+	
+	@Override
+	public boolean onCommand(CommandSender sender, Command cmd, String label,
+			String[] args) {
+		return commandHandler.onCommand(sender, cmd, label, args);
+	}
 }
